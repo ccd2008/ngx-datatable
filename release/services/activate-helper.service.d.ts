4896716179f62c35d034a2ca31975acdc35be441
Christopher Davis <mini-fate@Christophers-MacBook-Pro.local>
2019-06-21 14:06:48 -0500

    current master

--- release/services/activate-helper.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { TableActivateEventType } from '../types';
-/**
- *   Manages activate events
- */
-export declare class ActivateHelperService {
-    private _allowedEvents;
-    constructor(_allowedEvents: TableActivateEventType[]);
-    isAllowed(eventName: TableActivateEventType): boolean;
-}
--- release/services/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './scrollbar-helper.service';
-export * from './dimensions-helper.service';
-export * from './column-changes.service';
-export * from './activate-helper.service';
